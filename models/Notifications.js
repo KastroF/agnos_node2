f9ed447c09a10d76c32875357adc0083cf134391
KastroF <nkastrro@gmail.com>
2024-12-12 13:09:47 +0100

    make it better

---
+++ models/Notifications.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose"); 
+
+const notifSchema = mongoose.Schema({
+    
+    title: {type: String}, 
+    body: {type: String}, 
+    org_id: {type: mongoose.Schema.Types.ObjectId, ref: "User"}, 
+    fcmToken: {type: Array}, 
+    date: {type: Date}, 
+    read: {type: Boolean}
+    
+})
+
+
+module.exports = mongoose.model("Notification", notifSchema);
---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose"); 
+
+const userSchema = mongoose.Schema({
+  
+  name: {type: String}, 
+  email: {type: String}, 
+  password: {type: String}, 
+  profile: {type: String}, 
+  app: {type: String}, 
+  status: {type: String}, 
+  sigle: {type: String}, 
+  active: {type: Boolean},
+  userActive: {type: Boolean},
+  fcmToken: {type: Array},
+  description: {type: String},
+  phone: {type: String}, 
+  certified: {type: Boolean}
+                                   
+})
+
+module.exports = mongoose.model('User', userSchema)
